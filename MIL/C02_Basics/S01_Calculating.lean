8e23175d92115e657f8477e650a86383ad4fe45e
Dylan Johnston <54882294+DylanJohnston@users.noreply.github.com>
2026-01-28 19:18:35 +0000

    C2S1 + exercises

--- MIL/C02_Basics/S01_Calculating.lean
+++ MIL/C02_Basics/S01_Calculating.lean
@@ -7,10 +7,14 @@ example (a b c : ℝ) : a * b * c = b * (a * c) := by
 
 -- Try these.
 example (a b c : ℝ) : c * b * a = b * (a * c) := by
-  sorry
+  rw [mul_comm c b] --lhs is now (b * c) * a
+  rw [mul_assoc b c a] --cant swap c and a yet, need to use assoc to get lhs = b * (c * a)
+  rw [mul_comm c a] --lhs is now b * (a * c)
 
 example (a b c : ℝ) : a * (b * c) = b * (a * c) := by
-  sorry
+  rw [← mul_assoc a b c] --lhs is now (a * b) * c
+  rw [mul_comm a b] --lhs is now (b * a) * c
+  rw [mul_assoc b a c] --lhs is now b * (a * c)
 
 -- An example.
 example (a b c : ℝ) : a * b * c = b * c * a := by
@@ -20,23 +24,31 @@ example (a b c : ℝ) : a * b * c = b * c * a := by
 /- Try doing the first of these without providing any arguments at all,
    and the second with only one argument. -/
 example (a b c : ℝ) : a * (b * c) = b * (c * a) := by
-  sorry
+  rw [mul_comm]
+  rw [mul_assoc]
 
 example (a b c : ℝ) : a * (b * c) = b * (a * c) := by
-  sorry
+  rw [mul_comm a]
+  rw [mul_comm a]
+  rw [mul_assoc b]
 
 -- Using facts from the local context.
 example (a b c d e f : ℝ) (h : a * b = c * d) (h' : e = f) : a * (b * e) = c * (d * f) := by
-  rw [h']
-  rw [← mul_assoc]
-  rw [h]
-  rw [mul_assoc]
+  rw [h'] --lhs is now a * (b * f) after subbing in e=f
+  rw [← mul_assoc] --lhs is now (a * b) * f
+  rw [h] --lhs is now (c * d) * f
+  rw [mul_assoc] --lhs is now c * (d * f) = rhs. Done.
 
 example (a b c d e f : ℝ) (h : b * c = e * f) : a * b * c * d = a * e * f * d := by
-  sorry
+  rw [mul_assoc a b c] --lhs is now (a * (b * c)) * d
+  rw [h] --lhs is now (a * (e * f)) * d
+  rw [← mul_assoc a e f] --lhs is now ((a * e) * f) * d = rhs
 
 example (a b c d : ℝ) (hyp : c = b * a - d) (hyp' : d = a * b) : c = 0 := by
-  sorry
+  rw [hyp]
+  rw [hyp']
+  rw [mul_comm a b]
+  rw [sub_self] --sub_self is the tactic that X-X=0
 
 example (a b c d e f : ℝ) (h : a * b = c * d) (h' : e = f) : a * (b * e) = c * (d * f) := by
   rw [h', ← mul_assoc, h, mul_assoc]
@@ -61,6 +73,11 @@ variable (a b c : ℝ)
 #check mul_assoc c a b
 #check mul_comm a
 #check mul_comm
+
+#check two_mul a
+#check add_mul a b c -- proof of (a+b)*c = a*c+b*c
+#check mul_add a b c -- proof of a*(b+c) = a*b + a*c
+#check add_assoc -- proof that for all a,b,c in additive semigroup, (a+b)+c = a+(b+c)
 
 end
 
@@ -96,11 +113,28 @@ end
 section
 variable (a b c d : ℝ)
 
+--pure rw proof
 example : (a + b) * (c + d) = a * c + a * d + b * c + b * d := by
-  sorry
+  rw [add_mul, mul_add, mul_add, add_assoc (a * c + a * d)]
+
+--same as above, but using calc to help structure
+example : (a + b) * (c + d) = a * c + a * d + b * c + b * d := by
+  calc
+    (a + b) * (c + d) = a*c + a*d + (b*c + b*d) := by
+      rw [add_mul, mul_add, mul_add]
+    _ = a*c + a*d + b*c + b*d := by
+      rw [add_assoc (a * c + a * d)]
 
 example (a b : ℝ) : (a + b) * (a - b) = a ^ 2 - b ^ 2 := by
-  sorry
+  calc
+    (a + b) * (a - b) = (a+b)*a - (a+b)*b := by
+      rw [mul_sub]
+    _ = a*a+b*a - a*b-b*b := by
+      rw [add_mul, add_mul, ← sub_sub]
+    _ = a*a - b*b := by
+      rw [mul_comm a b, ← add_sub (a*a), sub_self (b * a), add_zero]
+    _ = a^2 - b^2 := by
+      rw [pow_two, pow_two]
 
 #check pow_two a
 #check mul_sub a b c
