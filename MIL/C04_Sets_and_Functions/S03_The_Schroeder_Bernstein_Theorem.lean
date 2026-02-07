f233d4ec88763bd78b437ec4320d41ff6d052744
Dylan Johnston <54882294+DylanJohnston@users.noreply.github.com>
2026-02-07 11:06:34 +0000

    C4S3 done

--- MIL/C04_Sets_and_Functions/S03_The_Schroeder_Bernstein_Theorem.lean
+++ MIL/C04_Sets_and_Functions/S03_The_Schroeder_Bernstein_Theorem.lean
@@ -10,7 +10,12 @@ open Classical
 variable {α β : Type*} [Nonempty β]
 
 section
-variable (f : α → β) (g : β → α)
+variable (f : α → β) (g : β → α) (x : α)
+
+#check (invFun g : α → β)
+#check (leftInverse_invFun : Injective g → LeftInverse (invFun g) g)
+#check (leftInverse_invFun : Injective g → ∀ y, invFun g (g y) = y)
+#check (invFun_eq : (∃ y, g y = x) → g (invFun g x) = x)
 
 def sbAux : ℕ → Set α
   | 0 => univ \ g '' univ
@@ -28,10 +33,12 @@ theorem sb_right_inv {x : α} (hx : x ∉ sbSet f g) : g (invFun g x) = x := by
     rw [sbSet, mem_iUnion]
     use 0
     rw [sbAux, mem_diff]
-    sorry
+    constructor
+    trivial
+    exact hx
   have : ∃ y, g y = x := by
-    sorry
-  sorry
+    simp at this; exact this
+  apply invFun_eq; exact this
 
 theorem sb_injective (hf : Injective f) : Injective (sbFun f g) := by
   set A := sbSet f g with A_def
@@ -50,15 +57,34 @@ theorem sb_injective (hf : Injective f) : Injective (sbFun f g) := by
       rw [if_pos x₁A, if_neg x₂nA] at hxeq
       rw [A_def, sbSet, mem_iUnion] at x₁A
       have x₂eq : x₂ = g (f x₁) := by
-        sorry
+        rw [hxeq]
+        symm
+        apply sb_right_inv; exact x₂nA
       rcases x₁A with ⟨n, hn⟩
       rw [A_def, sbSet, mem_iUnion]
       use n + 1
       simp [sbAux]
       exact ⟨x₁, hn, x₂eq.symm⟩
-    sorry
+    apply hf
+    have fx_eq: f x₁ = f x₂ := by
+      calc
+        f x₁ = (if x₁ ∈ A then f x₁ else invFun g x₁) := by exact (if_pos x₁A).symm
+        _ = (if x₂ ∈ A then f x₂ else invFun g x₂) := by apply hxeq
+        _ = f x₂ := by exact (if_pos x₂A)
+    apply fx_eq
   push_neg at xA
-  sorry
+  have inv_g_eq : invFun g x₁ = invFun g x₂ := by
+    calc
+      invFun g x₁ = (if x₁ ∈ A then f x₁ else invFun g x₁) := by rw[if_neg xA.1]
+      _ = (if x₂ ∈ A then f x₂ else invFun g x₂) := by apply hxeq
+      _ = invFun g x₂ := by rw[if_neg xA.2]
+  calc
+    x₁ = g (invFun g x₁) := by
+      apply symm; apply sb_right_inv; apply xA.1
+    _ = g (invFun g x₂) := by rw [inv_g_eq]
+    _ = x₂ := by apply sb_right_inv _ _ xA.2
+
+set_option pp.showLetValues true
 
 theorem sb_surjective (hg : Injective g) : Surjective (sbFun f g) := by
   set A := sbSet f g with A_def
@@ -77,9 +103,12 @@ theorem sb_surjective (hg : Injective g) : Surjective (sbFun f g) := by
       exact ⟨n, xmem⟩
     rw [h_def, sbFun, if_pos this]
     apply hg hx
-
-  sorry
-
+  use g y
+  rw [h_def]; unfold sbFun
+  calc
+    (if g y ∈ sbSet f g then f (g y) else invFun g (g y)) = invFun g (g y) := by rw [if_neg gyA]
+    _ = y := by
+      apply leftInverse_invFun hg
 end
 
 theorem schroeder_bernstein {f : α → β} {g : β → α} (hf : Injective f) (hg : Injective g) :
