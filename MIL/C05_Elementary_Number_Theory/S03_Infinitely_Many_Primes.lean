61024c24c6f4aa72c0af3a7a5536cfb8c8da75ca
Dylan Johnston <54882294+DylanJohnston@users.noreply.github.com>
2026-02-08 13:59:51 +0000

    C5S3 done

--- MIL/C05_Elementary_Number_Theory/S03_Infinitely_Many_Primes.lean
+++ MIL/C05_Elementary_Number_Theory/S03_Infinitely_Many_Primes.lean
@@ -156,15 +156,24 @@ theorem primes_infinite' : ∀ s : Finset Nat, ∃ p, Nat.Prime p ∧ p ∉ s :=
     simp [s'_def]
     apply h
   have : 2 ≤ (∏ i ∈ s', i) + 1 := by
-    sorry
+    simp
+    apply Nat.succ_le_of_lt
+    apply Finset.prod_pos
+    intro i
+    rw[mem_s']
+    apply Nat.Prime.pos
   rcases exists_prime_factor this with ⟨p, pp, pdvd⟩
   have : p ∣ ∏ i ∈ s', i := by
-    sorry
+    apply Finset.dvd_prod_of_mem
+    rwa [mem_s']
   have : p ∣ 1 := by
     convert Nat.dvd_sub pdvd this
     simp
   show False
-  sorry
+  have : p =1 := by apply Nat.eq_one_of_dvd_one this
+  have : p ≠ 1 := by apply Nat.Prime.ne_one pp
+  contradiction
+
 theorem bounded_of_ex_finset (Q : ℕ → Prop) :
     (∃ s : Finset ℕ, ∀ k, Q k → k ∈ s) → ∃ n, ∀ k, Q k → k < n := by
   rintro ⟨s, hs⟩
@@ -201,8 +210,23 @@ theorem two_le_of_mod_4_eq_3 {n : ℕ} (h : n % 4 = 3) : 2 ≤ n := by
       rw [neq] at h
       norm_num at h
 
+#check Nat.div_dvd_of_dvd
+#check Nat.div_lt_self
+
 theorem aux {m n : ℕ} (h₀ : m ∣ n) (h₁ : 2 ≤ m) (h₂ : m < n) : n / m ∣ n ∧ n / m < n := by
-  sorry
+  constructor
+  apply Nat.div_dvd_of_dvd h₀
+  have n_pos : 0 < n := by
+    calc
+    0 < 2 := by apply zero_lt_two
+    _ ≤ m := by apply h₁
+    _ < n := by apply h₂
+  have m_gt_1: 1 < m := by
+    calc
+    1 < 2 := by apply one_lt_two
+    _ ≤ m := by apply h₁
+  apply Nat.div_lt_self n_pos m_gt_1
+
 theorem exists_prime_factor_mod_4_eq_3 {n : Nat} (h : n % 4 = 3) :
     ∃ p : Nat, p.Prime ∧ p ∣ n ∧ p % 4 = 3 := by
   by_cases np : n.Prime
@@ -221,14 +245,35 @@ theorem exists_prime_factor_mod_4_eq_3 {n : Nat} (h : n % 4 = 3) :
     apply mod_4_eq_3_or_mod_4_eq_3
     rw [neq, h]
   rcases this with h1 | h1
-  . sorry
-  . sorry
+  . rcases em (Nat.Prime m) with h|h
+    use m
+    rcases (ih m mltn h1 h) with ⟨q, ⟨q_prime,q_dvd_m,q_mod_4_eq_3⟩⟩
+    use q
+    constructor; apply q_prime
+    constructor
+    apply dvd_trans q_dvd_m mdvdn
+    apply q_mod_4_eq_3
+  . let aux_hypoth := aux mdvdn mge2 mltn
+    rcases em (Nat.Prime (n / m)) with ndmp|ndmnp
+    use n/m
+    exact ⟨ndmp, aux_hypoth.1, h1⟩
+    rcases (ih (n/m) aux_hypoth.2 h1 ndmnp) with ⟨q, ⟨q_prime,q_dvd_ndm,q_mod_4_eq_3⟩⟩
+    use q
+    constructor
+    apply q_prime
+    constructor
+    apply dvd_trans q_dvd_ndm aux_hypoth.1
+    apply q_mod_4_eq_3
+
 example (m n : ℕ) (s : Finset ℕ) (h : m ∈ erase s n) : m ≠ n ∧ m ∈ s := by
   rwa [mem_erase] at h
 
 example (m n : ℕ) (s : Finset ℕ) (h : m ∈ erase s n) : m ≠ n ∧ m ∈ s := by
   simp at h
   assumption
+
+#check Nat.dvd_add_iff_left
+#check Nat.dvd_sub'
 
 theorem primes_mod_4_eq_3_infinite : ∀ n, ∃ p > n, Nat.Prime p ∧ p % 4 = 3 := by
   by_contra h
@@ -242,16 +287,42 @@ theorem primes_mod_4_eq_3_infinite : ∀ n, ∃ p > n, Nat.Prime p ∧ p % 4 = 3
     exact ⟨p, pltn, pp, p4⟩
   rcases this with ⟨s, hs⟩
   have h₁ : ((4 * ∏ i ∈ erase s 3, i) + 3) % 4 = 3 := by
-    sorry
+    simp
   rcases exists_prime_factor_mod_4_eq_3 h₁ with ⟨p, pp, pdvd, p4eq⟩
   have ps : p ∈ s := by
-    sorry
+    rw [← hs p]
+    exact ⟨pp,p4eq⟩
   have pne3 : p ≠ 3 := by
-    sorry
+    intro peq3
+    have : p ∣ (4 * ∏ i ∈ erase s 3, i + 3) ∧ p ∣ 3 := by exact ⟨pdvd, dvd_of_eq peq3⟩
+    have : p ∣ (4 * ∏ i ∈ erase s 3, i) := by apply Nat.dvd_sub this.1 this.2
+    rcases (Nat.Prime.dvd_mul pp).mp this with pd4 | pdprod
+    have : 3 ∣ 4 := by
+      rw[peq3] at pd4; assumption
+    contradiction
+    have : p ∈ s.erase 3 := by
+      apply mem_of_dvd_prod_primes pp
+      simp
+      intro n n_neq_3 ns
+      rw [← hs] at ns
+      apply ns.1
+      apply pdprod
+    simp at this
+    rcases this with ⟨pne3,idc⟩
+    contradiction
   have : p ∣ 4 * ∏ i ∈ erase s 3, i := by
-    sorry
+    have : p ∈ erase s 3 := by
+      simp
+      exact ⟨pne3, ps⟩
+    apply Nat.dvd_mul_left_of_dvd
+    apply Finset.dvd_prod_of_mem
+    apply this
   have : p ∣ 3 := by
-    sorry
+    convert Nat.dvd_sub pdvd this
+    simp
   have : p = 3 := by
-    sorry
+    apply Nat.Prime.eq_of_dvd_of_prime
+    apply pp
+    apply Nat.prime_three
+    apply this
   contradiction
