cda1ddb11cd8e467bd47f28aed8a0aa050c1bb3d
Dylan Johnston <54882294+DylanJohnston@users.noreply.github.com>
2026-02-14 19:15:12 +0000

    C8S3 done

--- MIL/C08_Hierarchies/S03_Subobjects.lean
+++ MIL/C08_Hierarchies/S03_Subobjects.lean
@@ -18,8 +18,6 @@ instance [Monoid M] : SetLike (Submonoid₁ M) M where
   coe := Submonoid₁.carrier
   coe_injective' _ _ := Submonoid₁.ext
 
-
-
 example [Monoid M] (N : Submonoid₁ M) : 1 ∈ N := N.one_mem
 
 example [Monoid M] (N : Submonoid₁ M) (α : Type) (f : M → α) := f '' N
@@ -52,7 +50,6 @@ instance [Monoid M] : SubmonoidClass₁ (Submonoid₁ M) M where
   mul_mem := Submonoid₁.mul_mem
   one_mem := Submonoid₁.one_mem
 
-
 instance [Monoid M] : Min (Submonoid₁ M) :=
   ⟨fun S₁ S₂ ↦
     { carrier := S₁ ∩ S₂
@@ -69,7 +66,16 @@ def Submonoid.Setoid [CommMonoid M] (N : Submonoid M) : Setoid M  where
     refl := fun x ↦ ⟨1, N.one_mem, 1, N.one_mem, rfl⟩
     symm := fun ⟨w, hw, z, hz, h⟩ ↦ ⟨z, hz, w, hw, h.symm⟩
     trans := by
-      sorry
+      intro x y z
+      rintro ⟨t, ht, u, hu, x_equiv_y⟩
+      rintro ⟨v, hv, w, hw, y_equiv_z⟩
+      use t*v, ?_, w*u, ?_
+      calc
+        x * (t * v) = y * (u * v) := by simp [← mul_assoc, x_equiv_y]
+        _ = y * v * u := by rw [mul_comm u v, mul_assoc]
+        _ = z * (w * u) := by rw [y_equiv_z, mul_assoc]
+      apply N.mul_mem ht hv
+      apply N.mul_mem hw hu
   }
 
 instance [CommMonoid M] : HasQuotient M (Submonoid M) where
@@ -79,12 +85,35 @@ def QuotientMonoid.mk [CommMonoid M] (N : Submonoid M) : M → M ⧸ N := Quotie
 
 instance [CommMonoid M] (N : Submonoid M) : Monoid (M ⧸ N) where
   mul := Quotient.map₂ (· * ·) (by
-      sorry
-        )
+      intro a b a_cong_b c d c_cong_d
+      simp
+      rcases a_cong_b with ⟨t,tn,u,un,a_equiv_b⟩
+      rcases c_cong_d with ⟨v,vn,w,wn,c_equiv_d⟩
+      use t*v, ?_, u*w, ?_
+      show a * c * (t * v) = b * d * (u * w)
+      calc
+        a * c * (t * v) = a*t*c*v := by rw [← mul_assoc, mul_assoc a c t, mul_comm c t, ← mul_assoc]
+        _ = b*u*d*w := by rw [mul_assoc (a*t) c v, a_equiv_b, c_equiv_d, ← mul_assoc]
+        _ = b*d*(u*w) := by rw [mul_assoc b u d, mul_comm u d, ← mul_assoc, mul_assoc]
+      apply N.mul_mem tn vn
+      apply N.mul_mem un wn
+      )
   mul_assoc := by
-      sorry
+    rintro a b c
+    rcases Quotient.exists_rep a with ⟨x, hx⟩ -- for q in quotient, ∃ a, ⟦a⟧ = q
+    rcases Quotient.exists_rep b with ⟨y, hy⟩
+    rcases Quotient.exists_rep c with ⟨z, hz⟩
+    rw [← hx,← hy,← hz]
+    apply Quotient.sound --states x ≃ y → ⟦x⟧ = ⟦y⟧. Use this to lift the goal into M.
+    simp [mul_assoc]
   one := QuotientMonoid.mk N 1
   one_mul := by
-      sorry
+      intro a
+      rcases Quotient.exists_rep a with ⟨x, hx⟩
+      rw [← hx]
+      apply Quotient.sound; simp
   mul_one := by
-      sorry
+      intro a
+      rcases Quotient.exists_rep a with ⟨x, hx⟩
+      rw [← hx]
+      apply Quotient.sound; simp
